17d6e2020ed240856619ecaa3ea2b75d7fa3f142
1054636298 <1054636298@139.com>
2025-09-23 21:55:09 +0800

    修复HTML显示问题

--- src/main/java/com/shireyishunjian/html/HTMLUtils.java
+++ src/main/java/com/shireyishunjian/html/HTMLUtils.java
@@ -9,8 +9,29 @@ public class HTMLUtils {
               <base href="https://www.shireyishunjian.com/main/">
               <script src="data/cache/common.js" type="text/javascript"></script>
               <script src="data/cache/forum.js" type="text/javascript"></script>
+              <script src="data/cache/forum_viewthread.js" type="text/javascript"></script>
+              <script type="text/javascript">
+                window.onload = function() {
+                    new lazyload()
+                };
+              </script>
               <link rel="stylesheet" type="text/css" href="data/cache/style_25_common.css">
               <link rel="stylesheet" type="text/css" href="data/cache/style_25_forum_viewthread.css">
+              <style>
+                table {
+                    border-collapse: separate;
+                    text-indent: initial;
+                    line-height: normal;
+                    font-weight: normal;
+                    font-size: medium;
+                    font-style: normal;
+                    color: -internal-quirk-inherit;
+                    text-align: start;
+                    border-spacing: 2px;
+                    white-space: normal;
+                    font-variant: normal;
+                }
+              </style>
             """;
 
     public static void addHead(Document document){
